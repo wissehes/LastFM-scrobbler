21966ae2453da350ec4f631f79200ec15fcc700f
Wisse <wissehes@gmail.com>
2023-02-01 10:17:34 +0100

    Initial Commit

---
+++ Scrobbler for Last.fm/Scrobbler_for_Last_fmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrobbler_for_Last_fmApp.swift
+//  Scrobbler for Last.fm
+//
+//  Created by Wisse Hes on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Scrobbler_for_Last_fmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
