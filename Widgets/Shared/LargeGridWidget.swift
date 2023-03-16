fe46e461f98dd93bfab48a98bce838557d6dcb9e
Wisse <wissehes@gmail.com>
2023-03-16 10:46:21 +0100

    Create commonly used widget types

---
+++ Widgets/Shared/LargeGridWidget.swift
@@ -0,0 +1,20 @@
+//
+//  LargeGridWidget.swift
+//  WidgetsExtension
+//
+//  Created by Wisse Hes on 16/03/2023.
+//
+
+import SwiftUI
+
+struct LargeGridWidget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LargeGridWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        LargeGridWidget()
+    }
+}
---
+++ Widgets/Shared/MediumGridWidget.swift
@@ -0,0 +1,20 @@
+//
+//  MediumGridWidget.swift
+//  WidgetsExtension
+//
+//  Created by Wisse Hes on 16/03/2023.
+//
+
+import SwiftUI
+
+struct MediumGridWidget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MediumGridWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        MediumGridWidget()
+    }
+}
