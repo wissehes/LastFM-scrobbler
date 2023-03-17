74ec349d51ee9904304fcee1c8b9e513a6f87c0c
Wisse <wissehes@gmail.com>
2023-03-17 14:20:43 +0100

    Conform to WidgetEntry and use the shared views

--- Widgets/TopAlbumsWidget/TopAlbumsWidget.swift
+++ Widgets/TopAlbumsWidget/TopAlbumsWidget.swift
@@ -10,12 +10,14 @@ import WidgetKit
 import SwiftUI
 import Intents
 
-struct TopAlbumsEntry: TimelineEntry {
+struct TopAlbumsEntry: TimelineEntry, WidgetEntry {
     var date: Date
     let configuration: ConfigurationIntent
     var albums: [CollageImage]
     var username: String?
     var error: Error?
+    
+    var items: [CollageImage] { return self.albums }
     
     init(date: Date, configuration: ConfigurationIntent, albums: [CollageImage], username: String? = nil, error: Error? = nil) {
         self.date = date
@@ -105,144 +107,13 @@ struct TopAlbumsWidgetView: View {
     var body: some View {
         switch family {
         case .systemMedium:
-            medium
+            MediumGridWidget(type: .albums, entry: entry)
         case .systemLarge:
-            large
+            LargeGridWidget(type: .albums, entry: entry)
+        case .systemSmall:
+            SmallGridWidget(entry: entry)
         @unknown default:
-            medium
-        }
-    }
-    
-    var medium: some View {
-        ZStack {
-            //            LinearGradient(colors: [colors], startPoint: .bottomLeading, endPoint: .topTrailing)
-            ContainerRelativeShape()
-                .fill(.cyan.gradient)
-            
-            VStack(alignment: .center, spacing: 5) {
-                usernameText(text: entry.username)
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .padding(.top, 10)
-                
-                Spacer()
-            }
-            GeometryReader { geo in
-                HStack(alignment: .center, spacing: 5) {
-                    ForEach(entry.albums.prefix(3), id: \.title) { item in
-                        newItemView(item)
-                    }
-                }.padding()
-            }
-        }
-    }
-    
-    let gridItems: [GridItem] = [
-        .init(.flexible(), spacing: 10, alignment: .center),
-        .init(.flexible(), spacing: 10, alignment: .center),
-        .init(.flexible(), spacing: 10, alignment: .center)
-    ]
-    
-    var large: some View {
-        ZStack {
-            background
-            
-            VStack(alignment: .center, spacing: 0) {
-                usernameText(text: entry.username)
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 10)
-                    .padding(.bottom, 0)
-                Text(entry.configuration.Period.period.subtitle)
-                    .font(.subheadline)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 5)
-                
-                GeometryReader { geo in
-                    LazyVGrid(columns: gridItems, spacing: 10) {
-                        ForEach(Array(entry.albums.enumerated()), id: \.element.title) { index, item in
-                            largeItemView(item, index: index, geo: geo)
-                        }
-                    }
-                }.padding([.bottom, .horizontal], 30)
-            }
-        }
-    }
-    
-    func largeItemView(_ item: CollageImage, index: Int, geo: GeometryProxy) -> some View {
-        ZStack {
-            item.image
-                .resizable()
-                .scaledToFill()
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .frame(
-                    width: geo.size.width / 3 - 5,
-                    height: geo.size.width / 3 - 5
-                )
-            
-            Text("\(index + 1)")
-                .font(.system(.caption2, design: .rounded))
-                .fontWeight(.bold)
-                .lineLimit(1)
-                .multilineTextAlignment(.center)
-                .padding(.leading, 4)
-                .padding(.trailing, 4)
-                .background(.thinMaterial)
-                .clipShape(Circle())
-                .frame(maxWidth: 150, maxHeight: 150, alignment: .topLeading)
-                .padding([.leading, .top], 5)
-            
-            VStack {
-                Spacer()
-                
-                Text("\(item.scrobbles) plays")
-                    .font(.system(.caption2, design: .rounded))
-                    .lineLimit(1)
-                    .multilineTextAlignment(.center)
-                    .padding(.leading, 3)
-                    .padding(.trailing, 3)
-                    .background(.thinMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
-            }.padding(.bottom, 2.5)
-        }
-    }
-    
-    func newItemView(_ item: CollageImage) -> some View {
-        GeometryReader { geo in
-            ZStack {
-                item.image
-                    .resizable()
-                    .scaledToFill()
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .frame(
-                        width: geo.size.width,
-                        height: geo.size.width
-                    )
-                
-                VStack {
-                    Spacer()
-                    
-                    Text("\(item.scrobbles) plays")
-                        .font(.system(.caption2, design: .rounded))
-                        .lineLimit(1)
-                        .multilineTextAlignment(.center)
-                        .padding(.leading, 3)
-                        .padding(.trailing, 3)
-                        .background(.thinMaterial)
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                        .padding(.bottom, 10)
-                }
-            }
-        }
-    }
-    
-    @ViewBuilder
-    func usernameText(text: String?) -> some View {
-        if let text = text {
-            Text("\(text)'s most listened albums")
-        } else {
-            Text("Your most listened albums")
+            MediumGridWidget(type: .albums, entry: entry)
         }
     }
 }
@@ -253,7 +124,8 @@ struct TopAlbumsWidget: Widget {
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: TopAlbumsProvider()) { entry in
             TopAlbumsWidgetView(entry: entry)
-        }.supportedFamilies([.systemMedium, .systemLarge])
+        }
+        //.supportedFamilies([.systemMedium, .systemLarge])
         .configurationDisplayName("Top Albums")
         .description("Shows your most listened albums.")
     }
